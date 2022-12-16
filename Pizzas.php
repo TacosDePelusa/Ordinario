fc33edf44299a01a569e28eb2cc3ff9b8a66e10e
Antonio Navarro <an147032@gmail.com>
2022-12-16 03:45:38 -0600

    Quinto commit

--- Pizzas.php
+++ Pizzas.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pizza y Corre</title>
-    <link rel="stylesheet" href="CSS/Estilo.css"/>
+    <title>Pizzas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+          rel="stylesheet"
+         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+        crossorigin="anonymous">
+        <link rel="stylesheet" href="CSS/Estilo.css"/>
 </head>
 <body class="cuerpo">
-    <header>
+<header>
         <nav class = "navigator" >
                 <ul class="nav-menu">
                     <li>
@@ -29,18 +33,145 @@
                         <a href="Productos.php" class="nav-menu-link nav-link ">Productos</a>
                     </li>
                     <li class="nav-itemn2">
-                        <a href="Pizzas.php" class="nav-menu-link nav-link  ">Pizzas</a>
+                        <a href="Principal.php" class="nav-menu-link nav-link  ">Pizzas</a>
                     </li>
                     <li class="nav-itemn2">
-                        <a href="Pastas.php" class="nav-menu-link nav-link ">Pastas</a>
+                        <a href="Productos.php" class="nav-menu-link nav-link ">Pastas</a>
                     </li>
                     <li class="nav-itemn2">
-                        <a href="Postres.php" class="nav-menu-link nav-link ">Postres</a>
+                        <a href="Carrito.php" class="nav-menu-link nav-link ">Postres</a>
                     </li>
                     <li class="nav-itemn2">
                         <a href="Carrito.php" class="nav-menu-link nav-link ">Carrito</a>
                     </li>
                 </ul>
         </nav>
+<br>
+<br>
+<main>
+<div class="container">
+ <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+ <div class="col">
+          <div class="card shadow-sm">
+            <img src="IMG/Pierna.jpg" >
+            <div class="card-body">
+              <p class="card-title">Pizza de Pierna (Tomate, Queso, Jamon y Pierna)</p>
+              <p class="card-title">Pizza de Pierna- Grande</p>
+              <p class="card-text">$130.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <p class="card-title">Pizza de Pierna- Mediana</p>
+              <p class="card-text">$120.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <p class="card-title">Pizza de Pierna- Chica</p>
+              <p class="card-text">$110.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+           
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div> <div class="col">
+          <div class="card shadow-sm">
+            <img src="IMG/Cama.jpg" >
+            <div class="card-body">
+              <p class="card-title">Pizza de Camarones (Tomate, Queso, Camaron)</p>
+              <p class="card-title">Pizza de Pierna- Grande</p>
+              <p class="card-text">$130.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <p class="card-title">Pizza de Pierna- Mediana</p>
+              <p class="card-text">$120.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <p class="card-title">Pizza de Pierna- Chica</p>
+              <p class="card-text">$110.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <div class="d-flex justify-content-between align-items-center">
+                
+                <div class="btn-group">
+              
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div> <div class="col">
+          <div class="card shadow-sm">
+            <img src="IMG/Jamon.jpg" >
+            <div class="card-body">
+              <p class="card-title">Pizza de Jamon Ahumado con Champiñones (Tomate, Queso, Jamon Ahumado y Champiñones)</p>
+              <p class="card-title">Pizza de Pierna- Grande</p>
+              <p class="card-text">$130.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <p class="card-title">Pizza de Pierna- Mediana</p>
+              <p class="card-text">$120.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <p class="card-title">Pizza de Pierna- Chica</p>
+              <p class="card-text">$110.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+               
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div> 
+        
+        <div class="col">
+          <div class="card shadow-sm">
+            <img src="IMG/Tocino.jpg" >
+            <div class="card-body">
+              <p class="card-title">Pizza de Tocino (Tomate, Queso, Jamon y Tocino)</p>
+              <p class="card-title">Pizza de Pierna- Grande</p>
+              <p class="card-text">$130.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <p class="card-title">Pizza de Pierna- Mediana</p>
+              <p class="card-text">$120.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <p class="card-title">Pizza de Pierna- Chica</p>
+              <p class="card-text">$110.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <img src="IMG/Chile.jpg" >
+            <div class="card-body">
+              <p class="card-title">Pizza de Tocino y Chile dulce (Tomate, Queso, Tocino y Chile dulce)</p>
+              <p class="card-title">Pizza de Pierna- Grande</p>
+              <p class="card-text">$130.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <p class="card-title">Pizza de Pierna- Mediana</p>
+              <p class="card-text">$120.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <p class="card-title">Pizza de Pierna- Chica</p>
+              <p class="card-text">$110.00</p>
+              <a href="" class="btn btn-success">agregar</a>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
+</main>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
+        crossorigin="anonymous"></script>
 </body>
 </html>
