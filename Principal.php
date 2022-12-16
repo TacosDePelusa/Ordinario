450ae2e33931bebd6756c0c52232e9acc8cfdb3c
Antonio Navarro <an147032@gmail.com>
2022-12-16 01:29:24 -0600

    Segundo commit

--- Principal.php
+++ Principal.php
@@ -18,16 +18,37 @@
                         <a href="Principal.php" class="nav-menu-link nav-link  ">Inicio</a>
                     </li>
                     <li class="nav-itemn">
-                        <a href="Productos.php" class="nav-menu-link nav-link ">Productos</a>
-                    </li>
-                    <li class="nav-itemn">
-                        <a href="Carrito.php" class="nav-menu-link nav-link ">Carrito</a>
-                    </li>
-                    <li class="nav-itemn">
                         <a href="Registro.php" class="nav-menu-link nav-link ">Registro</a>
                     </li>
                 </ul>
         </nav>
     </header>
+    <nav class = "navigator2" >
+                <ul class="nav-menu">
+                <li class="nav-itemn2">
+                        <a href="Productos.php" class="nav-menu-link nav-link ">Productos</a>
+                    </li>
+                    <li class="nav-itemn2">
+                        <a href="Principal.php" class="nav-menu-link nav-link  ">Pizzas</a>
+                    </li>
+                    <li class="nav-itemn2">
+                        <a href="Productos.php" class="nav-menu-link nav-link ">Pastas</a>
+                    </li>
+                    <li class="nav-itemn2">
+                        <a href="Carrito.php" class="nav-menu-link nav-link ">Postres</a>
+                    </li>
+                    <li class="nav-itemn2">
+                        <a href="Carrito.php" class="nav-menu-link nav-link ">Carrito</a>
+                    </li>
+                </ul>
+        </nav>
+<br>
+<br>
+<p class="texto">
+ <h1 class="Titulo">Acerca de Nosotros</h1>
+Creada en 1991 después de que el dueño emigrará de Canadá y al observar la pizzerías extranjeras
+decidió fundar un pizzería que funcionara en México, fue
+cuando inauguró la primera sucursal en la colonia maría luisa.
+</p>
 </body>
 </html>
