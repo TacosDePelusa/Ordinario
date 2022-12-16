4e28aa995d53abbe3ebf784e44594bd3c0f2cf67
Antonio Navarro <an147032@gmail.com>
2022-12-15 23:51:29 -0600

    Primer Commit

---
+++ Carrito.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizza y Corre</title>
+    <link rel="stylesheet" href="CSS/Estilo.css"/>
+</head>
+<body class="cuerpo">
+<header>
+        <nav class = "navigator" >
+                <ul class="nav-menu">
+                    <li>
+                        <img src="IMG/Logo.jpg" class="logo"></a>
+                    </li>
+                    <li class="nav-itemn">
+                        <a href="Principal.php" class="nav-menu-link nav-link  ">Inicio</a>
+                    </li>
+                    <li class="nav-itemn">
+                        <a href="Productos.php" class="nav-menu-link nav-link ">Productos</a>
+                    </li>
+                    <li class="nav-itemn">
+                        <a href="Carrito.php" class="nav-menu-link nav-link ">Carrito</a>
+                    </li>
+                    <li class="nav-itemn">
+                        <a href="Registro.php" class="nav-menu-link nav-link ">Registro</a>
+                    </li>
+                </ul>
+        </nav>
+    </header>
+</body>
+</html>
